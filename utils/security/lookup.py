723d0d759b3caee4b60f2f95214681a883451722
olly <15944451+pressol@users.noreply.github.com>
2021-12-01 16:06:22 +0000

    added tests for lookup.py

--- utils/security/lookup.py
+++ utils/security/lookup.py
@@ -1,6 +1,9 @@
+
 def inlookuptable(value: str, lookuptable: str):
     with open(lookuptable, "r") as lookuptablestream:
-        for table in lookuptablestream.readline():
-            if table == value:
+        for table in lookuptablestream:
+            if table.strip("\n") == value:
                 return True
     return False
+
+
--- utils/tests/security/test_lookup.py
+++ utils/tests/security/test_lookup.py
@@ -1,6 +1,24 @@
+import urllib.request
 from unittest import TestCase
 
+from utils.security.lookup import inlookuptable
 
-class Test(TestCase):
-    def test_inlookuptable(self):
-        self.fail()
+
+class InLookupTable(TestCase):
+    def test_inlookuptable_inlist_pass(self):
+        wordlist = "/home/network/PycharmProjects/onos_cli/onos_controller_rest_libary/onosrest/types/wordlist.txt"
+        self.assertEqual(inlookuptable("zoom", wordlist), True)
+
+    def test_inlookuptable_inlist_pass_large(self):
+        # Download the file from `url` and save it locally under `file_name`:
+        with urllib.request.urlopen(
+                "https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt") as response, open(
+            "/tmp/rockup.txt", 'wb') as out_file:
+            data = response.read()  # a `bytes` object
+            out_file.write(data)
+        wordlist = "/tmp/rockup.txt"
+        self.assertEqual(inlookuptable("tekken5", wordlist), True)
+
+    def test_inlookuptable_inlist_pass_huge(self):  # this will take a long time
+        wordlist = "/home/network/Downloads/weakpass_3w"
+        self.assertEqual(inlookuptable("tekken5", wordlist), True)
