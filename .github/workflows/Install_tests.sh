44484fe7adb84d6d898f06fc9c4b01be37f29256
pressol <15944451+pressol@users.noreply.github.com>
2021-12-17 12:49:14 +0000

    Update Install_tests.sh

--- .github/workflows/Install_tests.sh
+++ .github/workflows/Install_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # install megacmd
-# Ubuntu version
+# get install package
 megacmd="https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/megacmd_1.4.0-5.1_amd64.deb"
+wget $megacmd
 # install 
-sudo apt install -y $megacmd
+sudo apt install -y megacmd_1.4.0-5.1_amd64.deb
